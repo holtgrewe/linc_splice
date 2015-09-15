724f8c07ab46b15b959fbdfa4daeff04129e5314
Kasia Wreczycka <katwre@gmail.com>
2015-09-15 15:07:38 +0200

    add output file

--- coverage/transcript_scores.R
+++ coverage/transcript_scores.R
@@ -33,7 +33,8 @@ binned.arithmfunc <- function(bins, numvar, mcolname, func)
   ## count e.g. sum of coverage within bins (windows)
   stopifnot(is(bins, "GRanges"))
   stopifnot(is(numvar, "RleList"))
-  stopifnot(identical(seqlevels(bins), names(numvar)))
+    
+  stopifnot(identical(sort(seqlevels(bins)), sort(names(numvar))))
   bins_per_chrom <- split(ranges(bins), seqnames(bins))
   means_list <- lapply(names(numvar),
                        function(seqname) {
@@ -49,27 +50,42 @@ binned.arithmfunc <- function(bins, numvar, mcolname, func)
 
 ### MAIN
 
+# read lincRNAs
+lincRNAs <- gffToGRanges(lincRNA_annotation_path)
+lincRNAs <- unlist(lincRNAs)
+lincRNAs = lincRNAs[lincRNAs$type=="transcript"]
+
 # read introns and exons
-txdb <- makeTxDbFromGFF(gene_annotation_path, format="gtf", circ_seqs=character())
-trak2_txs <- transcriptsBy(txdb, by="gene")
+#txdb <- makeTxDbFromGFF(gene_annotation_path, format="gtf", circ_seqs=character())
+#trak2_txs <- transcriptsBy(txdb, by="gene")
 #introns <- intronsByTranscript(txdb, use.names=TRUE)
 #exons <- exonsBy(txdb, use.names=TRUE)
-saveRDS(trak2_txs, paste0(data_dir, "transcripts.rds"))
+#saveRDS(trak2_txs, paste0(data_dir, "transcripts.rds"))
 #saveRDS(introns, paste0(data_dir, "introns.rds"))
 #saveRDS(exons, paste0(data_dir, "exons.rds"))
 exons <- readRDS(paste0(data_dir, "exons.rds"))
-introns <- readRDS(paste0(data_dir, "introns.rds"))
+#introns <- readRDS(paste0(data_dir, "introns.rds"))
+#introns_transcript_id = names(introns)
+
+#a <- mclapply(introns[1:100],function(ranges){if(length(ranges)) ranges$idx=1:length(ranges);ranges}, mc.cores=30)
+#b <- Reduce('c', a)
+
+#introns$intron_rank <- sapply(elementLengths(introns),seq,from=1)
+#saveRDS(introns, paste0(data_dir, "introns_ranks1.rds"))
+introns <- readRDS(paste0(data_dir, "introns_ranks1.rds"))
+
+#introns <- unlist(introns)
+#introns$transcript_id <-introns_name
+exons <- unlist(exons)
+exons$transcript_id <- names(exons)
 
 
-values(introns[[i]]) <- cbind(values(introns[[i]]), DataFrame( 1:length(introns[[i]]))) 
+#saveRDS(introns, paste0(data_dir, "introns_grl_unlist.rds"))
+#saveRDS(exons, paste0(data_dir, "exons_grl_inlist.rds"))
+#introns <- readRDS(paste0(data_dir, "introns_grl_unlist.rds"))
+#introns <- introns[ which(!is.na(introns$transcript_id)), ] #TODO:
+#exons <- readRDS(paste0(data_dir, "exons_grl_inlist.rds"))
 
-for(i in 1:length(introns)){
-
-    introns[[i]]$intron_rank <- 1:length(introns[[i]])
-}
-
-# read lincRNAs
-lincRNAs <- gffToGRanges(lincRNA_annotation_path)
 
 #read data from Marcel
 metadata <- read.table(metadata_path, 
@@ -91,10 +107,7 @@ metadata = metadata[which(metadata$amount=="unique"),]
 # only aortic smooth muscle cell
 metadata = metadata[which(metadata$Biosample.term.name=="aortic smooth muscle cell"),]
 
-# only first replicate
-#first_replicate = as.character(metadata$Derived.from)[1]
-#metadata = metadata[which(metadata$Derived.from==first_replicate),]
-
+metadata$id <- 1:nrow(metadata)
 
 
 ### calculate sum of coverage within introns, exons, lncRNAs
@@ -104,49 +117,139 @@ exp_acc = as.character(metadata$Experiment.accession)
 #derived_from = as.character(metadata$Derived.from)
 bw_files = paste0(data_dir, file_acc, ".bigWig")
 
-
-#only for 1st chromosome
-lincRNAs = lincRNAs[which(seqnames(lincRNAs)=="chr1"),]
-introns = introns[which(seqnames(introns)=="chr1"),]
-exons = exons[which(seqnames(exons)=="chr1"),]
-
-chr1.size = seqinfo(BigWigFile(fil))["chr1"]
-chr1.gr <- GRanges(seqnames="chr1", ranges=IRanges(start=1, end=seqlengths(chr1.size)), strand="*")
-
-readBigWig.parallel <- function(i, targets=targets, windows=windows){
-    readBigWig(targets[i], windows=windows)$chr1
+readBigWig.parallel <- function(i, targets=targets){
+    #chr1.size = seqinfo(BigWigFile(fil))["chr1"]
+    #chr1.gr <- GRanges(seqnames="chr1", ranges=IRanges(start=1, end=seqlengths(chr1.size)), strand="*")
+    #readBigWig(targets[i], windows=chr1.gr)$chr1
+    readBigWig(targets[i])
 }    
-covs <- mclapply(1:nrow(metadata), 
-                  readBigWig.parallel,
-                  targets=bw_files,
-                  windows=chr1.gr,
-                  mc.cores=10
-                  )
+#covs <- mclapply(1:nrow(metadata), 
+#                  readBigWig.parallel,
+#                  targets=bw_files,
+#                  mc.cores=30
+#                  )
+#names(covs) <- file_acc
+#saveRDS(covs, paste0(data_dir, "covs.rds"))
+#covs <- readRDS(paste0(data_dir, "covs.rds"))
+
+normalize = function(cov, nmbr){
+  # for each chromosome separetly
+  #TODO: too slow
+  for(i in 1:length(cov)){
+    cov[[i]] <- cov[[i]] / nmbr  
+  }
+  cov
+}
+
+
+## normalize 
+aa <- unique(metadata$Derived.from)
+for(i in 1:length(aa)){
+   print(i)
+   samples_both_strands <- metadata [ metadata$Derived.from==aa[i] ,]
+   sum_covs <- sum(sapply(covs[samples_both_strands$id], sum))
+   covs[[samples_both_strands$id[1]]] <- normalize(covs[[samples_both_strands$id[1]]], sum_covs)
+   covs[[samples_both_strands$id[2]]] <- normalize(covs[[samples_both_strands$id[2]]], sum_covs)
+}
+
+
+#saveRDS(covs, paste0(data_dir, "covs_normalized.rds"))
+covs <- readRDS(paste0(data_dir, "covs_normalized.rds"))
 names(covs) <- file_acc
 
+
+
+#old
 #normalization. Samples from every strands are normalized by sum of coverage from both strands
-sum_covs <- sum(sapply(covs, sum)) #sum within derived from
-covs <- lapply(1:length(covs), function(x) covs[[x]] / sum_covs)
-names(covs) <- file_acc
+#sum_covs <- sum(sapply(covs, sum)) #sum within derived from
+#covs <- lapply(1:length(covs), function(x) covs[[x]] / sum_covs)
+#names(covs) <- file_acc
 
 
-introns <- unlist(introns)
-introns$transcript_id <- names(introns)
-exons <- unlist(exons)
-exons$transcript_id <- names(exons)
-lincRNAs <- unlist(introns)
+# for each replicate
+metadata.tmp <- metadata
+metadata <- metadata[,c("File.accession", "Output.type","strand",
+		      "amount", "id", "Biosample.term.name", "Experiment.accession", "Derived.from")]
 
+		      
+binned.arithmfunc <- function(bins, numvar, mcolname, func)
+{
+  ## count e.g. sum of coverage within bins (windows)
+  stopifnot(is(bins, "GRanges"))
 
-  i=1
+  bins_per_chrom <- split(ranges(bins), seqnames(bins))
+  means_list <- lapply(names(numvar),
+                       function(seqname) {
+                         views <- Views(numvar[[seqname]],
+                                        bins_per_chrom[[seqname]])
+                         func(views)
+                       })
+  mcols(bins)[[mcolname]] <- unlist(means_list)[names(bins)]
+  bins
+}		      
+		      
+
+names(lincRNAs) = lincRNAs$transcript_id	      
+		      
+for(i in 1:length(metadata$File.accession)){
+
+  print(i)
   introns <- binned.arithmfunc(introns, covs[[i]], names(covs)[i], viewSums)
   exons <- binned.arithmfunc(exons, covs[[i]], names(covs)[i], viewSums)
   lincRNAs <- binned.arithmfunc(lincRNAs, covs[[i]], names(covs)[i], viewSums)
+  
+  #normalization
+  mcols(introns)[,names(covs)[i]] <- mcols(introns)[,names(covs)[i]]*1. / sum(mcols(introns)[,names(covs)[i]])
+  mcols(exons)[,names(covs)[i]] <- mcols(exons)[,names(covs)[i]]*1. / sum(mcols(exons)[,names(covs)[i]])
+  mcols(lincRNAs)[,names(covs)[i]] <- mcols(lincRNAs)[,names(covs)[i]]*1. / sum(mcols(lincRNAs)[,names(covs)[i]])
+
+}
+
+#which_covs = which(names(covs[[i]]) %in% seqlevels(lincRNAs))
+#covs_lincRNAs <- covs[which_covs]
+#lincRNAs <- binned.arithmfunc(lincRNAs, covs[[i]], names(covs)[i], viewSums)
 
 
+# sum samples on + and - strand
+#(introns$transcript_id)
+#metadata$Derived.from
+
+introns.tmp = introns
+exons.tmp = exons
 
 
+introns_ave <- data.frame(1:length(introns))
+exons_ave <- data.frame(1:length(exons))
+lincRNA_ave <- data.frame(1:length(lincRNAs))
+
+aa <- as.character(unique(metadata$Derived.from))
+for(i in 1:length(aa)){
+   print(i)
+   samples_both_strands <- as.character(metadata [ metadata$Derived.from==aa[i] ,]$File.accession)
+   #introns
+   x1 <- rowSums(as.data.frame(mcols(introns)[samples_both_strands]), na.rm = TRUE)
+   y1 <- rowSums(as.data.frame(mcols(exons)[samples_both_strands]), na.rm = TRUE)
+   z1 <- rowSums(as.data.frame(mcols(lincRNAs)[samples_both_strands]), na.rm = TRUE)
+
+   introns_ave <- cbind(introns_ave, x1)
+   exons_ave <- cbind(exons_ave, y1)
+   lincRNA_ave <- cbind(lincRNA_ave, z1)
+   #values(introns) <- cbind(values(introns), DataFrame(o=xx))
+}
+
+introns$average = rowMeans(introns_ave)
+exons$average = rowMeans(exons_ave)
+lincRNAs$average = rowMeans(lincRNA_ave)
 
 
+# take average of replicates within sample
+df.introns = data.frame(txid=introns$transcript_id, type="intron", idx=introns$intron_rank, expression=introns$average)
+df.exons = data.frame(txid=exons$transcript_id, type="exon", idx=exons$exon_rank, expression=exons$average)
+df.lincRNA = data.frame(txid=lincRNAs$transcript_id, type="lincRNA", idx=0, expression=lincRNAs$average)
+
+
+df <- rbind(df.introns, df.exons, df.lincRNA)
+write.table(df, paste0(data_dir, "expression_aortic_smooth_muscle_cell.txt"))
     
     
     
